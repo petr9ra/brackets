5948409c88d7e154bcba3f71d2450d4c12f350d3
Petr <petr9ra@yandex.ru>
2021-08-24 15:01:58 +0300

    feat: add solution for this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let stack = [];
+
+    if (str.length % 2 !== 0){
+        return false
+    }
+
+    for (let i = 0; i < str.length; i++) {
+
+        let activeSymbol = str[i];
+
+        for (let j = 0; j < bracketsConfig.length; j++) {
+
+            let topElement = stack[stack.length - 1];
+
+            if (activeSymbol === bracketsConfig[j][1] && topElement === bracketsConfig[j][0]) {
+                stack.pop();
+            } else if (activeSymbol === bracketsConfig[j][0]) {
+                stack.push(activeSymbol);
+            }
+        }
+    }
+    return stack.length === 0 ? true : false;
+};
